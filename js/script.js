20a43caf0e975ffddc41cbb8913444829e2c0106
shoaibraza7201 <shoaibraza7201@gmail.com>
2021-09-14 15:03:27 +0530

    small changes

--- js/script.js
+++ js/script.js
@@ -1,1 +1,56 @@
-console.log("hello world");
+// var apple = 80 + 20 + 5555 + 5488;
+// document.write(apple);
+
+// var chop = 8;
+// chop += 6;
+
+// document.write(chop);
+
+// var x = 5;
+// var y = 35;
+
+// if(x!=y){
+//     document.write("yes i you sure");
+// }
+
+// if(2==12){
+//     document.write("Enter the value ");
+// }else{
+//     document.write("enter second value");
+// }
+
+// var first = "Shoaib";
+// var second = "Raza";
+
+// if(  (first=="Shoaib")  ||  (second=="Raza") ){
+//     document.write("Hello shoaib ssss");
+// }
+
+// var boys = "kkkk";
+
+// switch (boys) {
+//   case "ali":
+//     document.write("ali name is print");
+//     break;
+
+//   case "raza":
+//     document.write("raza name is print");
+//     break;
+
+//   default:
+//     document.write("name is Empty");
+// }
+
+
+// for(i = 0; i < 10; i+=3)
+// {
+//     document.write("Enter the value");
+//     document.write("<br>")
+// }
+
+var x = 1;
+
+while (x<10){
+    document.write(x + "enter the while value                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               "); 
+
+}
